59110086790e3aaea85a3ccba7077aebf6cf66f1
QingFeng-awa <qingfeng_awa@163.com>
2026-03-24 15:40:56 +0800

    更新baseUrl

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -14,7 +14,7 @@ import { defineUserConfig } from 'vuepress'
 import { plumeTheme } from 'vuepress-theme-plume'
 
 export default defineUserConfig({
-  base: '/',
+  base: '/TONX-Doc-Source/',
   lang: 'zh-CN',
   locales: {
     '/': {
