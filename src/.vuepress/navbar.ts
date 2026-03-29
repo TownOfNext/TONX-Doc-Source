ad99d5a889a309e8d4643f82bac1c6ba26195530
QingFeng-awa <qingfeng_awa@163.com>
2026-03-30 04:48:52 +0800

    增加nav图标

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -7,20 +7,17 @@
 import { defineNavbarConfig } from 'vuepress-theme-plume'
 
 export const zhNavbar = defineNavbarConfig([
-  { text: '首页', link: '/' },
-  { text: "模组介绍", link: "/docs/introduction" },
-  { text: "下载安装", link: "/docs/getting-started" },
-  { text: "进阶教程", link: "/docs/advanced" },
-  { text: "职业列表", link: "/docs/roles" },
-  { text: "常见问题", link: "/docs/faq" },
+  { icon: "fluent:info-24-regular", text: "模组介绍", link: "/docs/introduction" },
+  { icon: "fluent:arrow-download-24-regular", text: "下载安装", link: "/docs/getting-started" },
+  { icon: "fluent:book-24-regular", text: "进阶教程", link: "/docs/advanced" },
+  { icon: "fluent:text-bullet-list-square-24-regular", text: "职业列表", link: "/docs/roles" },
+  { icon: "fluent:question-circle-24-regular", text: "常见问题", link: "/docs/faq" },
 ])
 
 export const enNavbar = defineNavbarConfig([
-  { text: 'Home', link: '/en/' },
-  { text: "About", link: "/en/docs/introduction" },
-  { text: "Installation", link: "/en/docs/getting-started" },
-  { text: "Advanced", link: "/en/docs/advanced" },
-  { text: "Roles", link: "/en/docs/roles" },
-  { text: "FAQ", link: "/en/docs/faq" },
+  { icon: "fluent:info-24-regular", text: "About", link: "/en/docs/introduction" },
+  { icon: "fluent:arrow-download-24-regular", text: "Install", link: "/en/docs/getting-started" },
+  { icon: "fluent:book-24-regular", text: "Advanced", link: "/en/docs/advanced" },
+  { icon: "fluent:text-bullet-list-square-24-regular", text: "Roles", link: "/en/docs/roles" },
+  { icon: "fluent:question-circle-24-regular", text: "FAQ", link: "/en/docs/faq" },
 ])
-
